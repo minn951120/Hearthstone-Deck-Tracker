d392d6d69c06382412d223572fbdd4109ca9c36a
Alexander Zeier <alex@hearthsim.net>
2018-03-03 21:12:01 -0700

    Handle missing oauth scopes

--- Hearthstone Deck Tracker/Core.cs
+++ Hearthstone Deck Tracker/Core.cs
@@ -152,7 +152,7 @@ namespace Hearthstone_Deck_Tracker
 			if(Helper.HearthstoneDirExists && Config.Instance.StartHearthstoneWithHDT && !Game.IsRunning)
 				Helper.StartHearthstoneAsync().Forget();
 
-			if(HSReplayNetOAuth.IsAuthenticated)
+			if(HSReplayNetOAuth.IsAuthenticatedForAnything())
 				HSReplayNetHelper.UpdateAccount().Forget();
 
 			Initialized = true;
@@ -160,7 +160,7 @@ namespace Hearthstone_Deck_Tracker
 			Influx.OnAppStart(
 				Helper.GetCurrentVersion(),
 				newUser,
-				HSReplayNetOAuth.IsAuthenticated,
+				HSReplayNetOAuth.IsFullyAuthenticated,
 				(int)(DateTime.UtcNow - _startUpTime).TotalSeconds,
 				PluginManager.Instance.Plugins.Count
 			);
